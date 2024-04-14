1e6ba397cf7e52c9936c8d7edd995d52950d389c
mayasdaydreams <pricemaya0@gmail.com>
2024-04-14 11:01:04 -0700

    everything looks like it needs to be committed again

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "Maya's Daydreams",
+	url: "https://mayasdaydreams.com/",
+	language: "en",
+	description: "this is where i hang out on the internet",
+	author: {
+		name: "Maya",
+		email: "youremailaddress@example.com",
+		url: "https://mayasdaydreams.com/about/"
+	}
+}
