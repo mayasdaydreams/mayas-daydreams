21711d1adff574a6063b677daac8abe9ccf43c86
mayasdaydreams <pricemaya0@gmail.com>
2024-11-02 11:37:09 -0700

    updated metadata

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,10 +1,10 @@
 export default {
-	title: "Eleventy Base Blog v9",
-	url: "https://example.com/",
+	title: "Maya's Daydreams",
+	url: "https://mayasdaydreams.com/",
 	language: "en",
 	description: "I am writing about my experiences as a naval navel-gazer.",
 	author: {
-		name: "Your Name Here",
+		name: "Maya",
 		email: "youremailaddress@example.com",
 		url: "https://example.com/about-me/"
 	}
